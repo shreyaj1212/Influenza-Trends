fed17e92f9b5ce4d0cae40dd043d149250bd825e
Shreya <shreyyajain@gmail.com>
2019-08-02 08:36:32 -0400

    yesterday's work

--- tweet_mining.py
+++ tweet_mining.py
@@ -1,24 +1,24 @@
-############ IMPORTS ##################################
+#Import the necessary methods from tweepy library
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
 #Variables that contains the user credentials to access Twitter API 
-access_token = "shreyajain1212"
-access_token_secret = "ENTER YOUR ACCESS TOKEN SECRET"
-consumer_key = "ENTER YOUR API KEY"
-consumer_secret = "ENTER YOUR API SECRET"
+access_token = "sjain@mxschool.edu"
+access_token_secret = "Pyazz12@"
+consumer_key = "sjain@mxschool.edu"
+consumer_secret = "Pyazz12"
 
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
-        print data
+        print( data)
         return True
 
     def on_error(self, status):
-        print status
+        print( status)
 
 
 if __name__ == '__main__':
@@ -30,4 +30,4 @@ if __name__ == '__main__':
     stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['influenza', 'flu', 'sick'])
+    stream.filter(track=['influenza', 'flu'])
